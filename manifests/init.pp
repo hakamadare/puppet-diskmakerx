a97a45d623f901fef5e431c24164e257beccf4bc
Steve Huff <steve.huff@runkeeper.com>
2014-12-02 11:22:15 -0500

    first try at DiskMaker X module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,4 +1,11 @@
 # This is a placeholder class.
-class template {
-  anchor { 'Hello_World': }
+class diskmakerx (
+  $version = '4b4',
+) {
+  anchor  { 'begin': } ->
+  package { 'DiskMaker X.app':
+    provider => 'appdmg',
+    source   => "http://penful.com/cdn/DiskMakerX${version}.dmg",
+  } ->
+  anchor  { 'end': }
 }
--- spec/classes/diskmakerx_spec.rb
+++ spec/classes/diskmakerx_spec.rb
@@ -2,8 +2,11 @@ require 'spec_helper'
 # Rename this file to classname_spec.rb
 # Check other boxen modules for examples
 # or read http://rspec-puppet.com/tutorial/
-describe 'template' do
+describe 'diskmakerx' do
   it do
-    should contain_anchor('Hello_World')
+    should contain_anchor('begin')
+    should contain_anchor('end')
+
+    should contain_package('DiskMaker X.app')
   end
 end
